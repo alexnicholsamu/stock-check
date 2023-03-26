1344eaaf95f7b54f9623af531cb38a21aff11fdc
alexnicholsamu <alexander.k.nichols@gmail.com>
2023-03-26 16:09:54 -0400

    stock-dashboard

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+# Flask configuration
+SECRET_KEY = os.environ.get("SECRET_KEY") or ""
+
+# Flask-Mail configuration
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USE_TLS = False
+MAIL_USERNAME = os.environ.get("STOCK_MAIL_USERNAME") or ""
+MAIL_PASSWORD = os.environ.get("STOCK_MAIL_PASSWORD") or ""
+MAIL_DEFAULT_SENDER = ("Stock Dashboard", MAIL_USERNAME)
