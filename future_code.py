67aec7ea44ff229b45e1c74319d4dd7dc6ef173a
alexnicholsamu <alexander.k.nichols@gmail.com>
2023-04-13 12:30:32 -0400

    future_code planning

--- future_code.py
+++ future_code.py
@@ -1,1 +1,5 @@
-# used to have the summary bot here, but I used it. Future ideas will be put here
+# use gpt to give me model to predict future stock prices, using kaggle dataset (this would be hard, would need to
+# train it on a specific csv, that would be difficult and involve downloading a ton, or can train it on a total dataset
+# which would make the model less accurate) then use model to predict stock outcome
+
+# future future code would be involving the sentiment analysis in those predictions
